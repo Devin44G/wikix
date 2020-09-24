e82fbb20cee6c945eafb49f2a5eaf65dd689b64e
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-09-24 12:22:47 -0500

    Resourced routes for Projects

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +21,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [HomeController::class, 'index']);
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::resource('projects', ProjectController::class);

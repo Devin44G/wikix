bc98b9dd9e8a1719826b838c6e3b3676bd3f5a66
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-09-24 12:21:59 -0500

    Created Model and migration for Story Docs

---
+++ database/migrations/2020_09_24_141528_create_stories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stories', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('story_doc_title')
+                  ->nullable();
+            $table->mediumText('story_doc_content')
+                  ->nullable();
+            $table->foreignId('project_id')
+                  ->constrained('projects')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stories');
+    }
+}

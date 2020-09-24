03469d0247d726fbca46def2168a9b84eece0936
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-09-24 12:21:34 -0500

    Created Model, Controller, and migration for Projects.

---
+++ database/migrations/2020_09_24_141345_create_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('project_name');
+            $table->string('project_description')
+                  ->nullable();
+            $table->foreignId('user_id')
+                  ->constrained('users')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}

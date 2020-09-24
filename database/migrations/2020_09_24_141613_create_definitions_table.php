638b71372126b1398ae906de5dd22da62f0ae5ad
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-09-24 12:21:02 -0500

    Created Model and migration for Definitions

---
+++ database/migrations/2020_09_24_141613_create_definitions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDefinitionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('definitions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('def_doc_word')
+                  ->nullable()
+                  ->unique();
+            $table->mediumText('def_doc_definition')
+                  ->nullable();
+            $table->foreignId('project_id')
+                  ->constrained('projects')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('definitions');
+    }
+}
